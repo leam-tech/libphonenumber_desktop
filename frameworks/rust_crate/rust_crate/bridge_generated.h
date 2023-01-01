e2e85dc1f5dee1a2f7143dd2bc35cc9d58037e8d
Kingkor Roy Tirtho <krtirtho@gmail.com>
2023-01-01 19:03:10 +0600

    fix: RustWireSync class not implement error and bump dependencies

--- frameworks/rust_crate/rust_crate/bridge_generated.h
+++ frameworks/rust_crate/rust_crate/bridge_generated.h
@@ -1,6 +1,9 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
+typedef struct _Dart_Handle* Dart_Handle;
+
+typedef struct DartCObject DartCObject;
 
 typedef int64_t DartPort;
 
@@ -11,13 +14,17 @@ typedef struct wire_uint_8_list {
   int32_t len;
 } wire_uint_8_list;
 
-typedef struct WireSyncReturnStruct {
-  uint8_t *ptr;
-  int32_t len;
-  bool success;
-} WireSyncReturnStruct;
+typedef struct DartCObject *WireSyncReturn;
 
 void store_dart_post_cobject(DartPostCObjectFnType ptr);
+
+Dart_Handle get_dart_object(uintptr_t ptr);
+
+void drop_dart_object(uintptr_t ptr);
+
+uintptr_t new_dart_opaque(Dart_Handle handle);
+
+intptr_t init_frb_dart_api_dl(void *obj);
 
 void wire_is_valid_number(int64_t port_,
                           struct wire_uint_8_list *phone_number,
@@ -33,7 +40,7 @@ void wire_get_region_info(int64_t port_,
 
 struct wire_uint_8_list *new_uint_8_list_0(int32_t len);
 
-void free_WireSyncReturnStruct(struct WireSyncReturnStruct val);
+void free_WireSyncReturn(WireSyncReturn ptr);
 
 static int64_t dummy_method_to_enforce_bundling(void) {
     int64_t dummy_var = 0;
@@ -41,7 +48,10 @@ static int64_t dummy_method_to_enforce_bundling(void) {
     dummy_var ^= ((int64_t) (void*) wire_normalize_phone_number);
     dummy_var ^= ((int64_t) (void*) wire_get_region_info);
     dummy_var ^= ((int64_t) (void*) new_uint_8_list_0);
-    dummy_var ^= ((int64_t) (void*) free_WireSyncReturnStruct);
+    dummy_var ^= ((int64_t) (void*) free_WireSyncReturn);
     dummy_var ^= ((int64_t) (void*) store_dart_post_cobject);
+    dummy_var ^= ((int64_t) (void*) get_dart_object);
+    dummy_var ^= ((int64_t) (void*) drop_dart_object);
+    dummy_var ^= ((int64_t) (void*) new_dart_opaque);
     return dummy_var;
 }
